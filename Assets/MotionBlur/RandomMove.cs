1f819eaa4200bda4f9fde070d214adf4916cedbc
張敦勇 <524547312@qq.com>
2020-09-15 14:31:58 +0800

    move_blue

---
+++ Assets/MotionBlur/RandomMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMove : MonoBehaviour
+{
+    float _time = 0;
+    Vector3 _target_pos = Vector3.zero;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_time <= 0)
+        {
+            _time = 1f;
+            _target_pos = new Vector3(Random.Range(0, 10f), Random.Range(0, 10f), Random.Range(0, 10f));
+        }
+
+        transform.position = Vector3.Lerp(_target_pos, transform.position, _time);
+        _time -= Time.deltaTime;
+    }
+}
